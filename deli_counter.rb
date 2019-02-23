53bee82898f4af804c282813512ef1caf1d7c14b
Jared Stromberg <jaredstromberg@gmail.com>
2019-02-23 03:41:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length > 0 
-    puts "The line is currently: " +
+    puts "The line is currently:" 
     katz_deli.each_with_index do |person, idx|
-      print (idx + 1) + ". " + person + " "
+      print " " + (idx + 1) + ". " + person + " "
     end
   else
     puts "The line is currently empty."
